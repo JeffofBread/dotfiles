f53d2094d365c66773f30618be118c95bfb11fca
JeffofBread <80713464+JeffofBread@users.noreply.github.com>
2025-04-15 19:20:15 -0400

    Misc updates

---
+++ .bashrc
@@ -0,0 +1,5 @@
+export VISUAL=nvim
+export EDITOR=nvim
+
+alias GMU_SSH='TERM=xterm && ssh USERNAME@zeus.cec.gmu.edu'
+alias GMU_VPN='nmcli --ask con up id GMU\ VPN'
--- jdwm/jdwm_autostart.sh
+++ jdwm/jdwm_autostart.sh
@@ -2,7 +2,7 @@
 
 # Below are just examples, copy or uncomment any you want. Note, make sure to add a '&' after each command
 
-dwmblocks &
+jdwmblocks &
 xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output HDMI-1 --mode 1920x1080 --rate 75.00 --pos 0x180 --rotate normal --output HDMI-2 --off --output DP-4 --primary --mode 2560x1440 --rate 240.00 --pos 1920x0 --rotate normal --output DP-5 --off &
 dunst &
 playerctld daemon &
@@ -12,3 +12,5 @@ flameshot &
 #jetbrains-toolbox --minimize &
 "$HOME"/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox --minimize &
 picom --animations -b &
+xset -dpms &
+xset s off &
