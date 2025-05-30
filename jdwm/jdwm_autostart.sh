99929274c22c03b32931dd080b03e4435edd6a70
JeffofBread <80713464+JeffofBread@users.noreply.github.com>
2025-05-29 20:28:10 -0400

    Update for new monitor setup

--- jdwm/jdwm_autostart.sh
+++ jdwm/jdwm_autostart.sh
@@ -1,9 +1,11 @@
-# Example file to be started by jdwm from autorun.h. For most programs, it's best to start them here rather than autorun.h
+#!/bin/sh
 
-# Below are just examples, copy or uncomment any you want. Note, make sure to add a '&' after each command
+# Example file to be started by jdwm.
+# Below are just examples, copy or uncomment any you want.
+# Note, make sure to add a '&' after each command
 
 jdwmblocks &
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output HDMI-1 --mode 1920x1080 --rate 75.00 --pos 0x180 --rotate normal --output HDMI-2 --off --output DP-4 --primary --mode 2560x1440 --rate 240.00 --pos 1920x0 --rotate normal --output DP-5 --off &
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output HDMI-1 --mode 1920x1080 --rate 75.00 --pos 0x270 --rotate normal --scale 1.5x1.5 --output HDMI-2 --off --output DP-4 --primary --mode 3840x2160 --rate 240.00 --pos 2880x0 --rotate normal --scale 1x1 --output DP-5 --off &
 dunst &
 playerctld daemon &
 pasystray &
@@ -11,6 +13,6 @@ nm-applet &
 flameshot &
 #jetbrains-toolbox --minimize &
 "$HOME"/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox --minimize &
-picom --animations -b &
+picom &
 xset -dpms &
 xset s off &
